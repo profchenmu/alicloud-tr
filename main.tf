385c045262b76509c09c2144526c5f9030f1b02d
Yuhong Lin <yuhong.lin@CNJIH1MV0QFQ05P.ap.jllnet.com>
2024-09-18 12:21:04 +0800

    fix

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ variable "region" {
   default = "cn-qingdao-b"
 }
 
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
+
 provider "alicloud" {
   access_key = var.access_key
   secret_key = var.secret_key
